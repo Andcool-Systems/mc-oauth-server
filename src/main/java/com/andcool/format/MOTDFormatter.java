e63b78eb617675f0c8ba032877d9501da63a45be
Andcool-Systems <drolll2007@gmail.com>
2024-06-04 22:08:19 +0300

    [InDev] Fixed bad code with MOTD Formatter

--- src/main/java/com/andcool/format/MOTDFormatter.java
+++ src/main/java/com/andcool/format/MOTDFormatter.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MOTDFormatter {
-    private static final char COLOR_CHAR = '&';
+    private static final String COLOR_CHAR = "\u00A7";
     private final Map<Character, String> colorMap = new HashMap<>();
     private final Map<Character, String> styleMap = new HashMap<>();
 
@@ -38,14 +38,13 @@ public class MOTDFormatter {
     }
 
     public JSONArray format(String motd) {
-        motd = motd.replace("§", "&");  // Because JAVA not working with '§' symbol. idk
         JSONArray jsonArray = new JSONArray();
         StringBuilder currentText = new StringBuilder();
         JSONObject currentJson = new JSONObject();
 
         for (int i = 0; i < motd.length(); i++) {
             char c = motd.charAt(i);
-            if (c == COLOR_CHAR && i + 1 < motd.length()) {
+            if (motd.startsWith(COLOR_CHAR, i + 1) && i + 1 < motd.length()) {
                 if (!currentText.isEmpty()) {
                     currentJson.put("text", currentText.toString());
                     jsonArray.put(currentJson);
@@ -53,14 +52,13 @@ public class MOTDFormatter {
                     currentJson = new JSONObject();
                 }
 
-                char formatChar = motd.charAt(i + 1);
+                char formatChar = motd.charAt(i + 2);
                 if (colorMap.containsKey(formatChar)) {
                     currentJson.put("color", colorMap.get(formatChar));
                 } else if (styleMap.containsKey(formatChar)) {
                     currentJson.put(styleMap.get(formatChar), true);
                 }
-
-                i++;
+                i += 2;
             } else {
                 currentText.append(c);
             }
